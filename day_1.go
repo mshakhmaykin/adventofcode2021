879f793b25f5aac4ddaac7f9b96e437abe237f25
Miroslav Shakhmaykin <mshakhmaykin@splunk.com>
2021-12-02 15:06:47 -0500

    Figure out input file from program file

--- day_1.go
+++ day_1.go
@@ -4,12 +4,22 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"runtime"
 	"strconv"
+	"strings"
 )
 
 func read_inputs() []int {
-	day := 1 // redo by capturing program filename
-	input_file := fmt.Sprintf("inputs/2021_%d.in", day)
+	var today_number int = 1
+	_, program_name, _, ok := runtime.Caller(1) // /some/path/adventofcode2021/day_1.go
+	if ok {
+		fields := strings.Split(program_name, "/")
+		base_name := fields[len(fields)-1]                 // day_1.go
+		drop_extension := strings.Split(base_name, ".")[0] // day_1
+		today_number, _ = strconv.Atoi(strings.Split(drop_extension, "_")[1])
+	}
+
+	input_file := fmt.Sprintf("inputs/2021_%d.in", today_number)
 	file, err := os.Open(input_file)
 	if err != nil {
 		fmt.Println(err)
