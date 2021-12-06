1ed11f45e22d206f78303bcc090f43e03a73eed4
Miroslav Shakhmaykin <mshakhmaykin@splunk.com>
2021-12-06 10:31:37 -0500

    Update previous commit, add test

--- day_3.go
+++ day_3.go
@@ -10,7 +10,44 @@ import (
 	"strings"
 )
 
-func read_inputs() map[int]map[string]int {
+func read_inputs1() map[int]map[string]int {
+	var today_number int = 1
+	_, program_name, _, ok := runtime.Caller(1) // /some/path/adventofcode2021/day_1.go
+	if ok {
+		fields := strings.Split(program_name, "/")
+		base_name := fields[len(fields)-1]                 // day_1.go
+		drop_extension := strings.Split(base_name, ".")[0] // day_1
+		today_number, _ = strconv.Atoi(strings.Split(drop_extension, "_")[1])
+	}
+
+	input_file := fmt.Sprintf("inputs/2021_%d_test.in", today_number)
+	file, err := os.Open(input_file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var inputs = make(map[int]map[string]int)
+
+	//for i := 0; i < 12; i++ { // initialize with the length of the string; cannot do in the next loop as it will recreate the map after each processed line
+	for i := 0; i < 5; i++ { // initialize with the length of the string; cannot do in the next loop as it will recreate the map after each processed line
+		inputs[i] = make(map[string]int)
+	}
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), "")
+		for idx, elem := range line {
+			inputs[idx][elem] += 1
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+	return inputs
+}
+
+func read_inputs2() []string {
 	var today_number int = 1
 	_, program_name, _, ok := runtime.Caller(1) // /some/path/adventofcode2021/day_1.go
 	if ok {
@@ -28,17 +65,10 @@ func read_inputs() map[int]map[string]int {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	var inputs = make(map[int]map[string]int)
-
-	for i := 0; i < 12; i++ { // initialize with the length of the string; cannot do in the next loop as it will recreate the map after each processed line
-		inputs[i] = make(map[string]int)
-	}
+	var inputs []string
 
 	for scanner.Scan() {
-		line := strings.Split(scanner.Text(), "")
-		for idx, elem := range line {
-			inputs[idx][elem] += 1
-		}
+		inputs = append(inputs, scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
@@ -46,7 +76,7 @@ func read_inputs() map[int]map[string]int {
 	return inputs
 }
 
-func step_1(values map[int]map[string]int) {
+func step_1(values map[int]map[string]int) ([]string, []string) {
 	var gamma []string
 	var epsilon []string
 	var keys []int
@@ -59,7 +89,7 @@ func step_1(values map[int]map[string]int) {
 
 	for idx := 0; idx < len(keys); idx++ {
 		elem := values[idx]
-		if elem["0"] >= elem["1"] {
+		if elem["0"] > elem["1"] {
 			gamma = append(gamma, "0")
 			epsilon = append(epsilon, "1")
 		} else {
@@ -75,10 +105,29 @@ func step_1(values map[int]map[string]int) {
 	epsilondec, _ := strconv.ParseInt(epsilonbin, 2, 16)
 	powercons := gammadec * epsilondec
 	fmt.Println(powercons)
+	return gamma, epsilon
 }
 
-func main() {
-	values := read_inputs()
+/*func step_2(gamma, epsilon []string, values []string) {
 	//fmt.Println(values)
-	step_1(values)
+	var oxlist = map[int]string{}
+
+	var co2list = map[int]string{}
+
+	for k := 0; k < 12; k++ {
+		common := gamma[k]
+		uncommon := epsilon[k]
+		for _, v := range values {
+			fmt.Println(strings.Split(v, "")[k])
+		}
+	}
+}*/
+
+func main() {
+	values1 := read_inputs1()
+	//	values2 := read_inputs2()
+	fmt.Println(values1)
+	g, e := step_1(values1)
+	fmt.Println(g, e)
+	//	step_2(g, e, values2)
 }
