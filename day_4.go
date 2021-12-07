1a7b584b9733fd0fc6722b725ac1ff979424443e
Miroslav Shakhmaykin <mshakhmaykin@splunk.com>
2021-12-07 00:03:56 -0500

    Day 4 Part 1 - test nums work

--- day_4.go
+++ day_4.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-func read_inputs() ([]string, [][][]string) {
+func read_inputs() ([]int, [][][]int) {
 	var today_number int = 1
 	_, program_name, _, ok := runtime.Caller(1) // /some/path/adventofcode2021/day_1.go
 	if ok {
@@ -30,17 +30,21 @@ func read_inputs() ([]string, [][][]string) {
 
 	var linecounter int
 	var cardcounter int
-	var cards [][][]string
+	var cards [][][]int
 	//cards := [][][]string{}
-	var lastcard [][]string
+	var lastcard [][]int
 
-	var drawnnums []string
+	var drawnnums []int
 
 	for scanner.Scan() {
 		linecounter += 1
 		line := scanner.Text()
 		if linecounter == 1 {
-			drawnnums = strings.Split(line, ",")
+			drawnnumsstr := strings.Split(line, ",")
+			for _, v := range drawnnumsstr {
+				n, _ := strconv.Atoi(v)
+				drawnnums = append(drawnnums, n)
+			}
 			//fmt.Println(drawnnums)
 			continue
 		}
@@ -51,14 +55,20 @@ func read_inputs() ([]string, [][][]string) {
 		//fmt.Println(linecounter)
 		if line != "" {
 			//fmt.Println(cardcounter)
-			lastcard = append(lastcard, strings.Split(line, " "))
+			tmp := strings.Fields(line)
+			lineint := []int{}
+			for _, v := range tmp {
+				n, _ := strconv.Atoi(v)
+				lineint = append(lineint, n)
+			}
+			lastcard = append(lastcard, lineint)
 			//fmt.Println(lastcard)
 		} else {
 			//fmt.Println("cards before", cards)
 			//fmt.Println("lastcard before", lastcard)
 			cards = append(cards, lastcard)
 			//fmt.Println(lastcard)
-			lastcard = make([][]string, 0)
+			lastcard = make([][]int, 0)
 			//fmt.Println("lastcard after", lastcard)
 			//fmt.Println("cards after", cards)
 			cardcounter += 1
@@ -68,13 +78,13 @@ func read_inputs() ([]string, [][][]string) {
 	return drawnnums, cards
 }
 
-func testcard(card [][]string) bool {
+func testcard(card [][]int) bool {
 	for i := 0; i < 5; i++ {
-		if card[i][0] == card[i][1] && card[i][1] == card[i][2] && card[i][2] == card[i][3] && card[i][3] == card[i][4] && card[i][4] == "X" {
+		if card[i][0] == card[i][1] && card[i][1] == card[i][2] && card[i][2] == card[i][3] && card[i][3] == card[i][4] && card[i][4] == -1 {
 			fmt.Println("winning here", card[i][0])
 			return true
 		}
-		if card[0][i] == card[1][i] && card[1][i] == card[2][i] && card[2][i] == card[3][i] && card[3][i] == card[4][i] && card[4][i] == "X" {
+		if card[0][i] == card[1][i] && card[1][i] == card[2][i] && card[2][i] == card[3][i] && card[3][i] == card[4][i] && card[4][i] == -1 {
 			fmt.Println("winning there", i)
 			fmt.Println(card[0][i], card[1][i], card[2][i], card[3][i], card[4][i])
 			fmt.Println(card)
@@ -84,24 +94,23 @@ func testcard(card [][]string) bool {
 	return false
 }
 
-func calculatecard(card [][]string, lastnum string) int {
+func calculatecard(card [][]int, lastnum int) int {
 	var sum int
 	var score int
 	for i := 0; i < 5; i++ {
 		for j := 0; j < 5; j++ {
-			if card[i][j] != "" {
-				num, _ := strconv.Atoi(card[i][j])
-				sum += num
+			if card[i][j] != -1 {
+				sum += card[i][j]
 			}
 		}
 	}
-	lastnumdec, _ := strconv.Atoi(lastnum)
-	score = sum * lastnumdec
+
+	score = sum * lastnum
 	//fmt.Println(score)
 	return score
 }
 
-func step_1(drawnnums []string, cards [][][]string) int {
+func step_1(drawnnums []int, cards [][][]int) int {
 	//fmt.Println(drawnnums)
 	//fmt.Println(cards[0][0])
 
@@ -111,7 +120,8 @@ func step_1(drawnnums []string, cards [][][]string) int {
 			for j := 0; j < 5; j++ {
 				for k := 0; k < 5; k++ {
 					if cards[i][j][k] == number {
-						cards[i][j][k] = "X"
+						cards[i][j][k] = -1
+						fmt.Println(cards[i])
 						if testcard(cards[i]) {
 							fmt.Println(cards[i], number)
 							return calculatecard(cards[i], number)
